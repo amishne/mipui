30ddc6957293714e8041f50606a023e61b65226d
Alon Mishne <alonmishne@gmail.com>
2017-09-20 10:06:22 -0400

    Sight gesture now supports multiple origin points and works per quarters and not octets.

--- public/app/sight_gesture.js
+++ public/app/sight_gesture.js
@@ -7,7 +7,12 @@ class SightGesture extends Gesture {
   startHover(cell) {
     if (!cell || cell.role != 'primary') return;
     this.hoveredCell_ = cell;
-    this.cellsInSight_ = this.calculateCellsInSight_(cell);
+    const origins = [{
+      x: cell.offsetLeft + cell.width / 2,
+      y: cell.offsetTop + cell.height / 2,
+      sectors: [{top: -1, bottom: 1}],
+    }];
+    this.cellsInSight_ = this.calculateCellsInSight_(cell, origins);
     this.cellsInSight_.forEach(cellInSight => {
       cellInSight.showHighlight(ct.overlay, {
         [ck.kind]: ct.overlay.hidden.id,
@@ -26,11 +31,11 @@ class SightGesture extends Gesture {
   continueGesture(cell) {}
   stopGesture() {}
 
-  getColumnCells_(origin, column, fromAngle, toAngle) {
+  getColumnCells_(origin, column) {
     const result = [];
     // Initial naive implementation.
     const firstRow = parseInt(state.getProperty(pk.firstRow)) - 0.5;
-    const lastRow = origin.row;
+    const lastRow = parseInt(state.getProperty(pk.lastRow)) + 0.5;// origin.row;
     for (let row = firstRow; row <= lastRow; row += 0.5) {
       row = Math.round(row * 2) / 2;
       const cell = state.theMap.getCell(row, column);
@@ -65,58 +70,37 @@ class SightGesture extends Gesture {
     return result;
   }
 
-  calculateCellsInSight_(origin) {
+  calculateCellsInSight_(originCell, originPoints) {
     const cellsInSight = [];
-    let currentSectors = [{
-      top: -1,
-      bottom: 0,
-    }];
-    const originOffset = {
-      top: origin.offsetTop,
-      bottom: origin.offsetTop + origin.height,
-      center: {
-        x: origin.offsetLeft + origin.width / 2,
-        y: origin.offsetTop + origin.height / 2,
-      },
-    };
     const maxColumn = parseInt(state.getProperty(pk.lastColumn)) + 0.5;
-    for (let column = origin.column; column <= maxColumn; column += 0.5) {
-      const nextSectors = [];
+    for (let column = originCell.column; column <= maxColumn; column += 0.5) {
       column = Math.round(column * 2) / 2;
-      const columnCells =
-          this.getColumnCells_(origin, column, currentSectors[0].top,
-              currentSectors[currentSectors.length - 1].bottom);
+      const columnCells = this.getColumnCells_(originCell, column);
       if (columnCells.length == 0) break;
-      const columnOffsetLeft = columnCells[0].offsetLeft;
-      const columnOffsetRight = columnOffsetLeft + columnCells[0].width;
+      const columnLeft = columnCells[0].offsetLeft;
+      const columnRight = columnLeft + columnCells[0].width;
       columnCells.forEach(columnCell => {
-        const cellOffsetTop = columnCell.offsetTop;
-        const cellOffsetBottom = columnCell.offsetTop + columnCell.height;
-        const isOpaque = this.isOpaque_(origin, columnCell);
-        currentSectors.forEach(currentSector => {
-          const sectorOffsetTop =
-              originOffset.center.y +
-              (columnOffsetRight - originOffset.center.x) * currentSector.top;
-          const sectorOffsetBottom =
-              originOffset.center.y +
-              (columnOffsetLeft - originOffset.center.x) * currentSector.bottom;
-          if (cellOffsetBottom >= sectorOffsetTop &&
-              cellOffsetTop <= sectorOffsetBottom) {
-            // The cell is visible.
-            cellsInSight.push(columnCell);
-          }
-//          if (!isOpaque) {
-//            nextSectors.push({
-//              top: (cellOffsetTop - originOffset.center.y) /
-//                  (columnOffsetRight - originOffset.center.x),
-//              bottom: (cellOffsetBottom - originOffset.center.y) /
-//                  (columnOffsetLeft - originOffset.center.x),
-//            });
-//          }
+        const cellIsAboveOrigin = columnCell.row <= originCell.row;
+        const sideClosestToSectorTop =
+            cellIsAboveOrigin ? columnRight : columnLeft;
+        const sideClosestToSectorBottom =
+            cellIsAboveOrigin ? columnLeft : columnRight;
+        const cellTop = columnCell.offsetTop;
+        const cellBottom = columnCell.offsetTop + columnCell.height;
+        originPoints.forEach(originPoint => {
+          const toCellTop =
+              (cellTop - originPoint.y) /
+              (sideClosestToSectorTop - originPoint.x);
+          const toCellBottom =
+              (cellBottom - originPoint.y) /
+              (sideClosestToSectorBottom - originPoint.x);
+          originPoint.sectors.forEach(sector => {
+            if (sector.top <= toCellTop && sector.bottom >= toCellBottom) {
+              cellsInSight.push(columnCell);
+            }
+          });
         });
       });
-      // currentSectors = this.mergeSectors_(nextSectors);
-      // if (currentSectors.length == 0) break;
     }
     return cellsInSight;
   }
