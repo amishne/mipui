7fc0e2373190e38a08463b0ba19bffec07d4cb29
Alon Mishne <alonmishne@gmail.com>
2017-12-02 08:15:08 -0500

    Fixed bug with order of operations in paint bucket tool.

--- public/app/paint_bucket_gesture.js
+++ public/app/paint_bucket_gesture.js
@@ -8,11 +8,11 @@ class PaintBucketGesture extends Gesture {
 
   startHover(cell) {
     this.stopHover();
+    const toWall = !cell.isKind(ct.walls, ct.walls.smooth);
+    this.magicWandGesture_.partialCellsConsideredFloor = toWall;
     this.magicWandGesture_.hoveredCell_ = cell;
     this.magicWandGesture_.anchorCell_ = null;
     this.magicWandGesture_.startGesture(cell);
-    const toWall = !cell.isKind(ct.walls, ct.walls.smooth);
-    this.magicWandGesture_.partialCellsConsideredFloor = toWall;
     const selectedCells = this.magicWandGesture_.selectedCells_;
     if (selectedCells.size == 0) return;
     this.wallGesture_.toWall = toWall;
