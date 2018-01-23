ce893eb6ac309a7d8db8f0e736e68a0510e5139f
Alon Mishne <alonmishne@gmail.com>
2018-01-23 08:17:25 -0500

    Workaround for a webkit issue clipping shadows inside foreignObjects.

--- public/app/cell.js
+++ public/app/cell.js
@@ -184,10 +184,14 @@ class Cell {
 
     // Set offsets on replicas.
     replicas.forEach(replica => {
-      replica.offsetLeft = 0;
-      replica.offsetRight = 0;
-      replica.offsetTop = 0;
-      replica.offsetBottom = 0;
+      // Using -0.1 instead of 0 because of a webkit bug related to
+      // https://bugs.chromium.org/p/chromium/issues/detail?id=804670
+      // In essence, this forces the tile to have at least a bit of actual
+      // presence instead the foreignObject, and so its shadow won't be cropped.
+      replica.offsetLeft = -0.1;
+      replica.offsetRight = -0.1;
+      replica.offsetTop = -0.1;
+      replica.offsetBottom = -0.1;
       if (this.tile.x < replica.tile.x) {
         replica.offsetLeft += replica.tile.width - this.tile.width;
         for (let x = this.tile.x + 1; x <= replica.tile.x; x++) {
