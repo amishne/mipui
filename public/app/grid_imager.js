ab4c4a7b710c427196a34826a524a72a0ed70ca5
Alon Mishne <alonmishne@gmail.com>
2018-02-20 08:06:17 -0500

    Async/await refactor for grid imager.

--- public/app/grid_imager.js
+++ public/app/grid_imager.js
@@ -18,48 +18,66 @@ class GridImager {
   recalculateStyleString() {
     this.styleString_ = Array.from(this.cssFiles_.entries())
         .map(([path, fileContent]) =>
-          `<style><!-- ${path} -->\n${fileContent}</style>`).join('\n');
+          `<style><!-- ${path} -->\n${fileContent}</style>`).join('\n')
+        .replace(/\s+/g, ' ');
   }
 
-  node2svgElement(node) {
-    return this.foreignObjectString2svgElement_(
-        this.xml2foreignObjectString_(this.node2xml_(node), false));
+  async node2svgElement(node) {
+    const xml = await this.node2xml_(node);
+    const foreignObjectString = await this.xml2foreignObjectString_(xml, false);
+    return await this.foreignObjectString2svgElement_(foreignObjectString);
   }
 
-  node2svgDataUrl(node, width, height) {
-    return this.foreignObjectString2svgDataUrl_(
-        this.xml2foreignObjectString_(this.node2xml_(node), true),
-        width, height);
+  async node2svgDataUrl(node, width, height) {
+    const xml = await this.node2xml_(node);
+    const foreignObjectString = await this.xml2foreignObjectString_(xml, true);
+    return await this.foreignObjectString2svgDataUrl_(
+        foreignObjectString, width, height);
   }
 
-  node2pngDataUrl(node, width, height) {
-    return this.canvas2dataUrl_(
-        this.imageElement2canvas_(
-            this.foreignObjectString2imageElement_(
-                this.xml2foreignObjectString_(this.node2xml_(node), true),
-                width, height),
-            width, height));
+  async node2pngDataUrl(node, width, height) {
+    const xml = await this.node2xml_(node);
+    const foreignObjectString = await this.xml2foreignObjectString_(xml, true);
+    const imageElement = await this.foreignObjectString2imageElement_(
+        foreignObjectString, width, height);
+    const canvas = await this.imageElement2canvas_(imageElement, width, height);
+    return await this.canvas2dataUrl_(canvas);
   }
 
   msSince_(start) {
     return `${Math.round(performance.now() - start)}ms`;
   }
 
-  node2xml_(node) {
+  async node2xml_(node) {
     const start = performance.now();
-    const result = new XMLSerializer().serializeToString(node);
+    const cloned = node.cloneNode(true);
+    debug(`node cloning done in ${this.msSince_(start)}`);
+    const documentInsertionStart = performance.now();
+    const cloneContainer = document.createElement('div');
+    cloneContainer.style.display = 'none';
+    node.parentElement.appendChild(cloneContainer);
+    cloneContainer.appendChild(cloned);
+    debug(`cloned node document insertion done in ${
+      this.msSince_(documentInsertionStart)}`);
+    const processStart = performance.now();
+    await this.processNode_(cloned);
+    debug(`node processing done in ${this.msSince_(processStart)}`);
+    const serializeStart = performance.now();
+    const result = new XMLSerializer().serializeToString(cloned);
+    debug(`node serialization done in ${this.msSince_(serializeStart)}`);
+    node.parentElement.removeChild(cloneContainer);
     debug(`node2xml_() done in ${this.msSince_(start)}`);
     return result;
   }
 
-  xml2foreignObjectString_(xml, includeStyle) {
+  async xml2foreignObjectString_(xml, includeStyle) {
     return '<foreignObject style="width:100%;height:100%">' +
       (includeStyle ? this.styleString_ : '') + xml + '</foreignObject>'
         .replace(/\n/g, ' ')
         .replace(/ +/g, ' ');
   }
 
-  foreignObjectString2svgElement_(foreignObjectString) {
+  async foreignObjectString2svgElement_(foreignObjectString) {
     const start = performance.now();
     const svgElement = document.createElement('svg');
     svgElement.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
@@ -68,36 +86,51 @@ class GridImager {
     return svgElement;
   }
 
-  foreignObjectString2svgDataUrl_(foreignObjectString, width, height) {
+  async foreignObjectString2svgDataUrl_(foreignObjectString, width, height) {
     const encodedForeignObjectString = encodeURIComponent(foreignObjectString);
     return 'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" ' +
         `width="${width}" height="${height}">${encodedForeignObjectString}` +
         '</svg>';
   }
 
-  foreignObjectString2imageElement_(foreignObjectString, width, height) {
+  async foreignObjectString2imageElement_(foreignObjectString, width, height) {
+    const svgDataUrl = await this.foreignObjectString2svgDataUrl_(
+        foreignObjectString, width, height)
+    return await this.dataUrl2imageElement_(svgDataUrl);
+  }
+
+  async dataUrl2imageElement_(dataUrl) {
     const imageElement = document.createElement('img');
-    imageElement.src = this.foreignObjectString2svgDataUrl_(
-        foreignObjectString, width, height);
+    imageElement.src = dataUrl;
     return imageElement;
   }
 
-  imageElement2canvas_(imageElement, width, height) {
-    const start = performance.now();
-    const canvas = document.createElement('canvas');
-    canvas.width = width;
-    canvas.height = height;
-    const context = canvas.getContext('2d');
-    context.drawImage(imageElement, 0, 0);
-    debug(`imageElement2canvas_() done in ${this.msSince_(start)}`);
-    return canvas;
+  async imageElement2canvas_(imageElement, width, height, callback) {
+    return new Promise((resolve, reject) => {
+      const start = performance.now();
+      const canvas = document.createElement('canvas');
+      canvas.width = width;
+      canvas.height = height;
+      const context = canvas.getContext('2d');
+      imageElement.onload = () => {
+        context.drawImage(imageElement, 0, 0);
+        debug(`imageElement2canvas_() done in ${this.msSince_(start)}`);
+        resolve(canvas);
+      }
+    });
   }
 
-  canvas2dataUrl_(canvas) {
+  async canvas2dataUrl_(canvas) {
     const start = performance.now();
     const pngDataUrl = canvas.toDataURL();
     debug(`canvas2dataUrl_() done in ${this.msSince_(start)}`);
     return pngDataUrl;
+  }
+
+  async svgDataUrl2pngDataUrl_(svgDataUrl, width, height) {
+    const imageElement = await this.dataUrl2imageElement_(svgDataUrl);
+    const canvas = await this.imageElement2canvas_(imageElement, width, height);
+    return await this.canvas2dataUrl_(canvas);
   }
 
   loadFile_(path, callback) {
@@ -108,5 +141,22 @@ class GridImager {
       callback(xhr.responseText);
     };
     xhr.send();
+  }
+
+  async processNode_(node) {
+    const backgroundImage =
+        getComputedStyle(node).backgroundImage.replace(/\\"/g, "'");
+    if (backgroundImage.includes('svg+xml')) {
+      const width = 200;//node.clientWidth;
+      const height = 20;//node.clientHeight;
+      const pngDataUrl = backgroundImage.replace(
+          /^url\(['"](data:image\/svg\+xml;utf8,<svg.+)['"]\)/g,
+          (match, svgDataUrl) =>
+            this.svgDataUrl2pngDataUrl_(svgDataUrl, width, height));
+      node.style.backgroundImage = `url("${pngDataUrl}")`;
+    }
+    for (let i = 0; i < node.childElementCount; i++) {
+      await this.processNode_(node.children[i]);
+    }
   }
 }
