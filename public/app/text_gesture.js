c4c00c9851197ece00e2a0f0085a96b6e58ef3b6
Alon Mishne <alonmishne@gmail.com>
2018-11-06 17:57:16 -0500

    Text gesture's textarea works with sideways text.

--- public/app/text_gesture.js
+++ public/app/text_gesture.js
@@ -61,19 +61,39 @@ class TextGesture extends BoxGesture {
   }
 
   setInputGeometry_(inputElement, startCell, initialContent) {
+    if (this.rotation_ && !inputElement.style.transform) {
+      const temp = inputElement.style.width;
+      inputElement.style.width = inputElement.style.height;
+      inputElement.style.height = temp;
+      let offset = '0px';
+      switch (this.rotation_) {
+        case 'rotated-90':
+          inputElement.style.transform = 'rotate(90deg)';
+          offset = inputElement.style.height;
+          break;
+        case 'rotated-270':
+          inputElement.style.transform = 'rotate(270deg)';
+          offset = inputElement.style.width;
+          break;
+      }
+      const halfOffset = Number.parseFloat(offset) / 2;
+      inputElement.style.transformOrigin = `${halfOffset}px ${halfOffset}px`;
+    }
     const startCellElement = startCell.getBaseElementAndMaybeCreateAllElements(
         this.getLayer_(), initialContent, false);
     if (!startCellElement) return;
-    inputElement.style.fontSize = startCellElement.style.fontSize;
+    inputElement.style.fontSize = startCellElement.children[0].style.fontSize;
     if (startCell.textHeight) {
-      const whitespace =
-          startCellElement.scrollHeight - startCell.textHeight;
+      const whitespace = !!this.rotation_ ?
+        startCellElement.scrollWidth - startCell.textHeight :
+        startCellElement.scrollHeight - startCell.textHeight;
       inputElement.style.paddingTop = (whitespace / 2 + 1) + 'px';
     }
   }
 
   createStartCellContent_() {
     const content = super.createStartCellContent_();
+    if (!content) return content;
     if (this.rotation_) {
       if (this.rotation_ == 'rotated-90') {
         content[ck.transform] = 'r90';
