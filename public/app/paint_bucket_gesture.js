79d1089aaef9e003ed7f0c6be33206b759a00d62
Alon Mishne <alonmishne@gmail.com>
2019-01-17 07:57:26 -0500

    Paint bucket now has a small delay, plus doesn't immediately rehover.

--- public/app/paint_bucket_gesture.js
+++ public/app/paint_bucket_gesture.js
@@ -5,32 +5,49 @@ class PaintBucketGesture extends Gesture {
     this.wallGesture_ = new WallGesture(1, true);
     this.cellsToSet_ = new Set();
     this.lastOp_ = state.getLastOpNum();
+    this.targetCell_ = null;
+    this.timer_ = null;
   }
 
   startHover(cell) {
     this.stopHover();
-    if (this.cellsToSet_.has(cell) && this.lastOp_ == state.getLastOpNum()) {
+    if (cell == this.targetCell_) {
+      return;
+    }
+    this.targetCell_ = cell;
+    if (this.timer_) {
+      clearTimeout(this.timer_);
+    }
+    this.timer_ = setTimeout(() => {
+      this.timer_ = null;
+      if (this.cellsToSet_.has(cell) && this.lastOp_ == state.getLastOpNum()) {
+        this.wallGesture_.startHoverAfterAllFieldsAreSet_();
+        return;
+      }
+      const toWall = !cell.isKind(ct.walls, ct.walls.smooth);
+      this.magicWandGesture_.partialCellsConsideredFloor = toWall;
+      this.magicWandGesture_.hoveredCell_ = cell;
+      this.magicWandGesture_.anchorCell_ = null;
+      this.magicWandGesture_.startGesture(cell);
+      this.cellsToSet_ = this.magicWandGesture_.selectedCells_;
+      this.lastOp_ = state.getLastOpNum();
+      if (this.cellsToSet_.size == 0) {
+        this.wallGesture_.cellsToSet = [];
+        return;
+      }
+      this.wallGesture_.toWall = toWall;
+      this.wallGesture_.cellsToSet = this.cellsToSet_;
       this.wallGesture_.startHoverAfterAllFieldsAreSet_();
-      return;
-    }
-    const toWall = !cell.isKind(ct.walls, ct.walls.smooth);
-    this.magicWandGesture_.partialCellsConsideredFloor = toWall;
-    this.magicWandGesture_.hoveredCell_ = cell;
-    this.magicWandGesture_.anchorCell_ = null;
-    this.magicWandGesture_.startGesture(cell);
-    this.cellsToSet_ = this.magicWandGesture_.selectedCells_;
-    this.lastOp_ = state.getLastOpNum();
-    if (this.cellsToSet_.size == 0) {
-      this.wallGesture_.cellsToSet = [];
-      return;
-    }
-    this.wallGesture_.toWall = toWall;
-    this.wallGesture_.cellsToSet = this.cellsToSet_;
-    this.wallGesture_.startHoverAfterAllFieldsAreSet_();
+    }, 30);
   }
 
   stopHover() {
-    this.wallGesture_.stopHover();
+    if (this.timer_) {
+      clearTimeout(this.timer_);
+      this.timer_ = null;
+    } else {
+      this.wallGesture_.stopHover();
+    }
   }
 
   startGesture() {
